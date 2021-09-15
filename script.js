9c4b0a18a77e1ed82918ae5319305cff53b5a6f4
Reeve-ch7 <reeve.cherian@gmail.com>
2021-09-15 14:58:16 +0530

    First commit

--- script.js
+++ script.js
@@ -20,6 +20,18 @@ submit.addEventListener("click", () => {
     (pSymbol.checked) ? initialPassword += symbol : "";
 
     password.value = generatePassword(pLength.value, initialPassword)
+    // database
+
+    db.collection('passwords')
+    .add({
+      password: password.value,
+    })
+    .then((docRef) => {
+      console.log('Password Stored');
+    })
+    .catch((error) => {
+      console.error('Error adding Password: ', error);
+    });
 });
 
 function generatePassword(l, initialPassword) {
@@ -43,3 +55,24 @@ copy.addEventListener("click", () => {
         alert("Password has been copied to clipboard");
     }
 });
+
+// Database connection
+
+const firebaseApp = firebase.initializeApp({
+    apiKey: "AIzaSyAJ1DJBtuQ983wEv9pyOC0M_RJiE9WRvTA",
+    authDomain: "password-generator-d8b52.firebaseapp.com",
+    projectId: "password-generator-d8b52",
+    storageBucket: "password-generator-d8b52.appspot.com",
+    messagingSenderId: "379761015812",
+    appId: "1:379761015812:web:7f61c21028adf640c74951",
+    measurementId: "G-SPSX5EEVWE"
+  });
+  const db = firebaseApp.firestore();
+//   db.collection('passwords')
+//     .get()
+//     .then((querySnapshot) => {
+//       querySnapshot.forEach((doc) => {
+//         // console.log(`${doc.id} => ${doc.data()}`);
+//         console.log(doc.data());
+//       });
+//     });
